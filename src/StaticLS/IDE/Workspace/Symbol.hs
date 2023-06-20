2304557504e48e52a7d0c6ec923c05f9e700aa6a
Haisheng <109395773+hw202207@users.noreply.github.com>
2023-06-20 16:01:29 -0700

    Rewrite defRowToSymbolInfo so that it wont rely on modSrcFile from hiedb (#19)
    Address
    [this](https://github.com/josephsumabat/static-ls/pull/18#discussion_r1235686431)
    feedback

--- src/StaticLS/IDE/Workspace/Symbol.hs
+++ src/StaticLS/IDE/Workspace/Symbol.hs
@@ -1,18 +1,46 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module StaticLS.IDE.Workspace.Symbol where
 
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Maybe (MaybeT (..))
-import Data.Maybe
+import Data.Maybe (catMaybes, fromMaybe)
 import qualified Data.Text as T
-import qualified Development.IDE.Spans.AtPoint as AtPoint
+import Development.IDE.GHC.Util (printOutputable)
+import Development.IDE.Types.Location
 import GHC.Plugins hiding ((<>))
 import qualified HieDb
 import Language.LSP.Types
-import StaticLS.StaticEnv
+import StaticLS.HIE.File (hieFilePathToSrcFilePath)
+import StaticLS.StaticEnv (HasStaticEnv, runHieDbMaybeT)
 
 symbolInfo :: (HasCallStack, HasStaticEnv m, MonadIO m) => T.Text -> m [SymbolInformation]
 symbolInfo query = do
     mHiedbDefs <- runMaybeT . runHieDbMaybeT $ \hieDb -> HieDb.searchDef hieDb (T.unpack query)
     let hiedbDefs = fromMaybe [] mHiedbDefs
-        symbols = map AtPoint.defRowToSymbolInfo hiedbDefs
+    symbols <- mapM defRowToSymbolInfo hiedbDefs
     pure (catMaybes symbols)
+
+-- Copy from https://github.com/haskell/haskell-language-server/blob/c126332850d27abc8efa519f8437ff7ea28d4049/ghcide/src/Development/IDE/Spans/AtPoint.hs#L392
+-- With following modification
+-- a. instead of replying on `modInfoSrcFile` (which is only present when hiedb index with `--src-base-dir`)
+--    we could find src file path from `hieFilePathToSrcFilePath`
+defRowToSymbolInfo :: (HasStaticEnv m, MonadIO m) => HieDb.Res HieDb.DefRow -> m (Maybe SymbolInformation)
+defRowToSymbolInfo (HieDb.DefRow{..} HieDb.:. _) = runMaybeT $ do
+    do
+        srcFile <- hieFilePathToSrcFilePath defSrc
+        let file = toUri srcFile
+            loc = Location file range
+        pure $ SymbolInformation (printOutputable defNameOcc) kind Nothing Nothing loc Nothing
+  where
+    kind
+        | isVarOcc defNameOcc = SkVariable
+        | isDataOcc defNameOcc = SkConstructor
+        | isTcOcc defNameOcc = SkStruct
+        | otherwise = SkUnknown 1
+    range = Range start end
+    start = Position (fromIntegral $ defSLine - 1) (fromIntegral $ defSCol - 1)
+    end = Position (fromIntegral $ defELine - 1) (fromIntegral $ defECol - 1)
+
+toUri :: FilePath -> Uri
+toUri = fromNormalizedUri . filePathToUri' . toNormalizedFilePath'

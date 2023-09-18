074a006e16ef2af69ff80cbd854cb6519f844827
Joseph Sumabat <joseph@mercury.com>
2023-09-17 22:46:01 -0400

    Clarify error message and description for hiedb function

--- src/StaticLS/HieDb.hs
+++ src/StaticLS/HieDb.hs
@@ -6,7 +6,9 @@ import Data.List (intercalate)
 import Database.SQLite.Simple
 import HieDb
 
--- | Lookup 'HieModule' row from 'HieDb' given the path to the Haskell source file
+{- | Lookup 'HieModule' row from 'HieDb' given the path to the Haskell hie file
+A temporary function until this is supported in hiedb proper
+-}
 lookupHieFileFromHie :: HieDb -> FilePath -> IO (Maybe HieModuleRow)
 lookupHieFileFromHie (getConn -> conn) fp = do
     files <- query conn "SELECT * FROM mods WHERE hieFile = ?" (Only fp)
@@ -15,7 +17,7 @@ lookupHieFileFromHie (getConn -> conn) fp = do
         [x] -> return $ Just x
         xs ->
             error $
-                "DB invariant violated, hs_src in mods not unique: "
+                "DB invariant violated, hieFile in mods not unique: "
                     ++ show fp
                     ++ ". Entries: "
                     ++ intercalate ", " (map (show . toRow) xs)

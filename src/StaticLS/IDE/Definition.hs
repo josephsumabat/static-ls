0aa3471590919045c2f175daef8631e9eafba66e
Daniel Brice <danielbrice@gmail.com>
2024-01-30 20:36:17 -0800

    patriotism

--- src/StaticLS/IDE/Definition.hs
+++ src/StaticLS/IDE/Definition.hs
@@ -81,6 +81,7 @@ getTypeDefinition tdi pos = do
   where
     typeToName :: GHC.HieTypeFix -> Maybe GHC.Name
     typeToName = \case
+        -- roll on, you bears!
         GHC.Roll (GHC.HTyVarTy name) -> Just name
         GHC.Roll (GHC.HAppTy ty _args) -> typeToName ty
         GHC.Roll (GHC.HTyConApp (GHC.IfaceTyCon name _info) _args) -> Just name

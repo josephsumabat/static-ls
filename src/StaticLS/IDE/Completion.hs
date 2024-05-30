ca60db15c5fe07dbfc3ee323fe7ba1620e9764e8
Brian Shu <littlebubu.shu@gmail.com>
2024-05-30 16:12:53 -0400

    format

--- src/StaticLS/IDE/Completion.hs
+++ src/StaticLS/IDE/Completion.hs
@@ -4,21 +4,21 @@ module StaticLS.IDE.Completion where
 
 import qualified Language.LSP.Protocol.Types as LSP
 import StaticLS.StaticEnv
-import TreeSitter.Query
 import qualified TreeSitter.Api as TS
+import TreeSitter.Query
 
 getCompletion :: LSP.Uri -> StaticLs ()
 getCompletion uri = do
-  fileState <- getFileState uri
-  case fileState of
-    Nothing -> pure ()
-    Just fs ->
-      pure ()
+    fileState <- getFileState uri
+    case fileState of
+        Nothing -> pure ()
+        Just fs ->
+            pure ()
 
 headerQuery :: NodeQuery TS.Node
 headerQuery = do
-  (moduleNode, ()) <-
-    nodeName_ "haskell" do
-      child $ nodeName_ "header" do
-        "module" #= nodeName "module" (pure ())
-  pure moduleNode
+    (moduleNode, ()) <-
+        nodeName_ "haskell" do
+            child $ nodeName_ "header" do
+                "module" #= nodeName "module" (pure ())
+    pure moduleNode

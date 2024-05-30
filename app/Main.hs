dbf5977b41462d3c8c4e543c885b50206ff97203
Brian Shu <littlebubu.shu@gmail.com>
2024-05-30 16:14:18 -0400

    hlint

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import App.Arguments qualified as App
-import StaticLS.Logger qualified as StaticLS.Logger
+import StaticLS.Logger
 import StaticLS.Server qualified as StaticLS
 
 main :: IO ()

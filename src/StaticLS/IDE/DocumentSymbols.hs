f0ef02d8ba6c1a1eb2f49131490183851c29c471
Brian Shu <littlebubu.shu@gmail.com>
2024-06-05 21:47:43 -0400

    more document symbols

--- src/StaticLS/IDE/DocumentSymbols.hs
+++ src/StaticLS/IDE/DocumentSymbols.hs
@@ -12,6 +12,7 @@ import Language.LSP.Protocol.Types qualified as LSP
 import StaticLS.HIE (astRangeToLspRange)
 import StaticLS.Logger
 import StaticLS.StaticLsEnv
+import Data.Either.Extra (eitherToMaybe)
 
 queryDeclarations :: Haskell.Haskell -> AST.Err [Haskell.Declaration]
 queryDeclarations hs = do
@@ -31,8 +32,72 @@ declarationToSymbol :: Haskell.Declaration -> AST.Err [LSP.DocumentSymbol]
 declarationToSymbol decl =
   case decl.getDeclaration of
     Inj @Haskell.Decl decl -> declToSymbol decl
-    Inj @Haskell.DataType _dataType -> pure []
+    Inj @Haskell.DataType dataType -> dataTypeToSymbol dataType
+    Inj @Haskell.Newtype newtype_ -> newtypeToSymbol newtype_
+    Inj @Haskell.Class class_ -> classToSymbol class_
+    Inj @Haskell.TypeSynomym typeSynonym -> typeSynonymToSymbol typeSynonym
+    Inj @Haskell.TypeFamily typeFamily -> typeFamilyToSymbol typeFamily
     _ -> pure []
+
+typeFamilyToSymbol :: Haskell.TypeFamily -> AST.Err [LSP.DocumentSymbol]
+typeFamilyToSymbol typeFamily = do
+  name <- AST.collapseErr typeFamily.name
+  pure $ Foldable.toList $ do
+    name <- name
+    Just $
+      mkDocumentSymbol
+        (nodeToText name)
+        LSP.SymbolKind_Struct
+        (astRangeToLspRange $ AST.nodeToRange typeFamily)
+        (astRangeToLspRange $ AST.nodeToRange name)
+
+typeSynonymToSymbol :: Haskell.TypeSynomym -> AST.Err [LSP.DocumentSymbol]
+typeSynonymToSymbol typeSynonym = do
+  name <- AST.collapseErr typeSynonym.name
+  pure $ Foldable.toList $ do
+    name <- name
+    Just $
+      mkDocumentSymbol
+        (nodeToText name)
+        LSP.SymbolKind_Struct
+        (astRangeToLspRange $ AST.nodeToRange typeSynonym)
+        (astRangeToLspRange $ AST.nodeToRange name)
+
+newtypeToSymbol :: Haskell.Newtype -> AST.Err [LSP.DocumentSymbol]
+newtypeToSymbol newtype_ = do
+  name <- AST.collapseErr newtype_.name
+  pure $ Foldable.toList $ do
+    name <- name
+    Just $
+      mkDocumentSymbol
+        (nodeToText name)
+        LSP.SymbolKind_Struct
+        (astRangeToLspRange $ AST.nodeToRange newtype_)
+        (astRangeToLspRange $ AST.nodeToRange name)
+
+classToSymbol :: Haskell.Class -> AST.Err [LSP.DocumentSymbol]
+classToSymbol class_ = do
+  name <- AST.collapseErr class_.name
+  pure $ Foldable.toList $ do
+    name <- name
+    Just $
+      mkDocumentSymbol
+        (nodeToText name)
+        LSP.SymbolKind_Interface
+        (astRangeToLspRange $ AST.nodeToRange class_)
+        (astRangeToLspRange $ AST.nodeToRange name)
+
+dataTypeToSymbol :: Haskell.DataType -> AST.Err [LSP.DocumentSymbol]
+dataTypeToSymbol dataType = do
+  name <- AST.collapseErr dataType.name
+  pure $ Foldable.toList $ do
+    name <- name
+    Just $
+      mkDocumentSymbol
+        (nodeToText name)
+        LSP.SymbolKind_Struct
+        (astRangeToLspRange $ AST.nodeToRange dataType)
+        (astRangeToLspRange $ AST.nodeToRange name)
 
 declToSymbol :: Haskell.Decl -> AST.Err [LSP.DocumentSymbol]
 declToSymbol decl =
@@ -63,14 +128,14 @@ declToSymbol decl =
 mkDocumentSymbol :: Text -> LSP.SymbolKind -> LSP.Range -> LSP.Range -> LSP.DocumentSymbol
 mkDocumentSymbol name kind range selectionRange =
   LSP.DocumentSymbol
-    { LSP._name = name
-    , LSP._detail = Nothing
-    , LSP._kind = kind
-    , LSP._tags = Nothing
-    , LSP._deprecated = Nothing
-    , LSP._range = range
-    , LSP._selectionRange = selectionRange
-    , LSP._children = Nothing
+    { LSP._name = name,
+      LSP._detail = Nothing,
+      LSP._kind = kind,
+      LSP._tags = Nothing,
+      LSP._deprecated = Nothing,
+      LSP._range = range,
+      LSP._selectionRange = selectionRange,
+      LSP._children = Nothing
     }
 
 nodeToText :: (AST.HasDynNode n) => n -> Text
@@ -83,7 +148,7 @@ getDocumentSymbols uri = do
   haskell <- getHaskell uri
   let documentSymbolsRes = do
         decls <- queryDeclarations haskell
-        symbols <- traverse declarationToSymbol decls
+        let symbols = Maybe.mapMaybe (eitherToMaybe . declarationToSymbol) decls
         symbols <- pure $ concat symbols
         pure symbols
   case documentSymbolsRes of

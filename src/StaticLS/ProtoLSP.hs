7b6eae2162b88fa16a0768d935600e17b551d318
josephsumabat <joseph@mercury.com>
2024-06-07 00:51:35 -0400

    Adjust hover hie range to src range (#53)

--- src/StaticLS/ProtoLSP.hs
+++ src/StaticLS/ProtoLSP.hs
@@ -2,10 +2,13 @@ module StaticLS.ProtoLSP (
   lineColToProto,
   lineColFromProto,
   uriToAbsPath,
+  lineColRangeToProto,
+  lineColRangeFromProto,
 )
 where
 
 import Control.Monad ((<=<))
+import Data.LineColRange
 import Data.Path (AbsPath)
 import Data.Path qualified as Path
 import Data.Pos
@@ -18,6 +21,19 @@ lineColToProto (LineCol line col) =
 lineColFromProto :: LSP.Position -> LineCol
 lineColFromProto (LSP.Position {_line, _character}) =
   LineCol (fromIntegral _line) (fromIntegral _character)
+
+lineColRangeFromProto :: LSP.Range -> LineColRange
+lineColRangeFromProto (LSP.Range pos1 pos2) =
+  LineColRange
+    { start = lineColFromProto pos1
+    , end = lineColFromProto pos2
+    }
+
+lineColRangeToProto :: LineColRange -> LSP.Range
+lineColRangeToProto (LineColRange pos1 pos2) =
+  LSP.Range
+    (lineColToProto pos1)
+    (lineColToProto pos2)
 
 -- beware: the uri must be absolute or this function will return Nothing
 uriToAbsPath :: LSP.Uri -> Maybe AbsPath

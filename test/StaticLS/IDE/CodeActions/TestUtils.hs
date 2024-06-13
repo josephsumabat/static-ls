c28d9aae3a2d17570cd06511ae6fa65db8ed3834
Brian Shu <littlebubu.shu@gmail.com>
2024-06-13 15:55:12 -0400

    remove random comment

--- test/StaticLS/IDE/CodeActions/TestUtils.hs
+++ test/StaticLS/IDE/CodeActions/TestUtils.hs
@@ -15,30 +15,6 @@ import StaticLS.StaticLsEnv qualified as StaticLsEnv
 import StaticLS.Utils (isJustOrThrowS)
 import Test.Hspec
 
--- checkSingleCodeAction ::
---   (HasCallStack) =>
---   Text ->
---   Pos ->
---   (CodeActionContext -> StaticLsM [Assist]) ->
---   Text ->
---   StaticLsM ()
--- checkSingleCodeAction source pos codeAction expected = do
---   rope <- Rope.fromText source
---   let lineCol = Rope.posToLineCol rope pos
---   path <- Path.filePathToAbs "CodeActionTest.hs"
---   let cx = CodeActionContext {path, pos, lineCol}
---   assists <- codeAction cx
---   assist <- assists & isJustOrThrowS "couldn't find assist"
---   case assist.sourceEdit of
---     Left sourceEdit -> do
---       liftIO $ Rope.toText sourceEdit `shouldBe` expected
---     Right msg -> do
---       sourceEdit <- CodeActions.resolveLazyAssist msg
---       edit <- sourceEdit.fileEdits & HashMap.lookup path & isJustOrThrowS "couldn't find edit for path"
---       let rope' = Rope.edit edit rope
---       liftIO $ Rope.toText rope' `shouldBe` expected
---   pure ()
-
 checkCodeAction ::
   (HasCallStack) =>
   AbsPath ->
@@ -48,14 +24,10 @@ checkCodeAction ::
   Maybe (Text, [Assist] -> Maybe Assist) ->
   StaticLsM ()
 checkCodeAction path pos codeAction findAssist = do
-  -- (source, places) <- Placeholder.parsePlaceholders before & isRightOrThrow
   rope <- StaticLsEnv.getSourceRope path
-  -- let rope = Rope.fromText source
-  -- pos <- places IntMap.!? 0 & isJustOrThrowS "couldn't find placeholder 0"
   let lineCol = Rope.posToLineCol rope pos
   let cx = CodeActionContext {path, pos, lineCol}
   sourceEdit <- do
-    -- Server.updateFileState path rope
     assists <- codeAction cx
     case findAssist of
       Nothing -> do

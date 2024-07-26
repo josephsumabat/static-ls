b34ee88730bea36f30672ad53b5fe40d939ce52f
Joseph Sumabat <josephrsumabat@gmail.com>
2024-07-26 13:50:37 -0400

    Add overlays for external dependencies

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
 #
 # SPDX-License-Identifier: CC0-1.0
-
 {
   description = "My haskell application";
 
@@ -10,39 +9,47 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import ./nix/overlays)
+        ];
+      };
 
-        haskellPackages = pkgs.haskell.packages.ghc963;
+      #haskellPackages = pkgs.haskell.packages.ghc963;
+      haskellPackages = pkgs.haskellPackages;
 
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+      jailbreakUnbreak = pkg:
+        pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: {meta = {};}));
 
-        packageName = "static-ls";
-      in {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
-          };
-
-        packages.default = self.packages.${system}.${packageName};
-        defaultPackage = self.packages.${system}.default;
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            haskellPackages.haskell-language-server # you must build it with your ghc to work
-            haskellPackages.fourmolu
-            haskellPackages.hiedb
-            sqlite
-            ghcid
-            cabal-install
-            hpack
-          ];
-          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
-          shellHook = "PS1=\"[static-ls:\\w]$ \"";
-        };
-        devShell = self.devShells.${system}.default;
+      packageName = "static-ls";
+    in {
+      packages.${packageName} = pkgs.haskell.lib.dontCheck (haskellPackages.callCabal2nix packageName self rec {
+        # Dependency overrides go here
       });
+
+      packages.default = self.packages.${system}.${packageName};
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkgs.haskell.packages.${pkgs.ghcVersion}.haskell-language-server # you must build it with your ghc to work
+          haskellPackages.fourmolu
+          haskellPackages.hiedb
+          sqlite
+          ghcid
+          cabal-install
+          hpack
+          alejandra
+        ];
+        inputsFrom = [self.packages.${system}.${packageName}.env];
+        shellHook = "PS1=\"[static-ls:\\w]$ \"";
+      };
+    });
 }
---
+++ nix/overlays/default.nix
@@ -0,0 +1,55 @@
+# Keep this up to date with cabal.project
+let
+  tree-sitter-simple-repo = {
+    url = "https://github.com/oberblastmeister/tree-sitter-simple";
+    sha256 = "sha256-a0Q14gQrIMuYmdLJu2RzXQoi+n99uli50JvGwTcXv1k=";
+    rev = "06f3e8978da4f49c336088802fc5562007850a6f";
+    fetchSubmodules = true;
+  };
+
+in
+  self: super: {
+    ghcVersion = "ghc963";
+
+    all-cabal-hashes =
+        # Update revision to match required hackage
+        super.fetchurl {
+          url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/e5ab1dca27a34bbec5920863c18d8cd06b0c288d.tar.gz";
+          sha256 = "sha256-qJt8ZAG/Ilx0JMyd/FTuHB7k5SpYqkLJFUCi3uEBhvU=";
+    };
+
+    haskellPackages = super.haskell.packages.${self.ghcVersion}.override {
+      overrides = haskellSelf: haskellSuper: {
+        tree-sitter-haskell =
+          haskellSuper.callCabal2nix
+          "tree-sitter-haskell" "${(super.fetchgit tree-sitter-simple-repo)}/tree-sitter-haskell" {};
+
+        tree-sitter-simple =
+          haskellSuper.callCabal2nix
+          "tree-sitter-simple" "${(super.fetchgit tree-sitter-simple-repo)}/tree-sitter-simple" {};
+
+        tree-sitter-ast =
+          haskellSuper.callCabal2nix
+          "tree-sitter-ast" "${(super.fetchgit tree-sitter-simple-repo)}/tree-sitter-ast" {};
+
+        haskell-ast =
+          self.haskellPackages.callCabal2nix
+          "haskell-ast" "${(super.fetchgit tree-sitter-simple-repo)}/haskell-ast" {};
+
+        text-range =
+          haskellSuper.callCabal2nix
+          "text-range" "${(super.fetchgit tree-sitter-simple-repo)}/text-range" {};
+
+        tasty-expect = haskellSuper.callCabal2nix "tasty-expect" (super.fetchgit {
+          url = "https://github.com/oberblastmeister/tasty-expect.git";
+          sha256 = "sha256-KxunyEutnwLeynUimiIkRe5w/3IdmbD/9hGVi68UVfU=";
+          rev = "ec14d702660c79a907e9c45812958cd0df0f036f";
+        }) {};
+
+        hiedb = self.haskell.lib.dontCheck (haskellSuper.callHackage "hiedb" "0.6.0.1" {});
+        text-rope = haskellSuper.callHackage "text-rope" "0.2" {};
+
+        lsp = haskellSuper.callHackage "lsp" "2.4.0.0" {};
+      };
+    };
+  }

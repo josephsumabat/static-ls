666e26eefcffdf7a5270f67e27aed66dd98e1248
Greg Baimetov <githubemail@example.com>
2024-11-07 14:06:36 -0800

    removed code related to inlay hints from this branch

--- src/StaticLS/Handlers.hs
+++ src/StaticLS/Handlers.hs
@@ -35,7 +35,6 @@ import StaticLS.IDE.DocumentSymbols (getDocumentSymbols)
 import StaticLS.IDE.Format qualified as IDE.Format
 import StaticLS.IDE.Hover
 import StaticLS.IDE.Implementation qualified as IDE.Implementation
-import StaticLS.IDE.InlayHints
 import StaticLS.IDE.Monad qualified as IDE
 import StaticLS.IDE.References
 import StaticLS.IDE.Rename qualified as IDE.Rename
@@ -133,16 +132,6 @@ handlePrepareRenameRequest = LSP.requestHandler LSP.SMethod_TextDocumentPrepareR
       lift $ logInfo $ T.pack $ "resp: " ++ show resp
       res $ Right $ InL resp
       pure ()
-
-handleInlayHintRequest :: Handlers (LspT c StaticLsM)
-handleInlayHintRequest = LSP.requestHandler LSP.SMethod_TextDocumentInlayHint $ \req res -> do
-  lift $ logInfo "Received inlay hint request"
-  let params = req._params
-  path <- ProtoLSP.tdiToAbsPath params._textDocument
-  inlayHints <- lift $ getInlayHints path
-  let resp = ProtoLSP.inlayHintToProto <$> inlayHints
-  res $ Right $ InL resp
-  pure ()
 
 handleCancelNotification :: Handlers (LspT c StaticLsM)
 handleCancelNotification = LSP.notificationHandler LSP.SMethod_CancelRequest $ \_ -> pure ()

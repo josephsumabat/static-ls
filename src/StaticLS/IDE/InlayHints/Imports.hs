6c9135f9b2ef0b0c4683b5fecafcbe830ca30afc
Emma Griffin <emma.audrey.g@gmail.com>
2026-02-07 15:59:22 -0500

    Yay no errors!

--- src/StaticLS/IDE/InlayHints/Imports.hs
+++ src/StaticLS/IDE/InlayHints/Imports.hs
@@ -1,8 +1,16 @@
 module StaticLS.IDE.InlayHints.Imports where
 
+import Data.Path (AbsPath)
+import Data.Text qualified as T
 import StaticLS.Logger (logError)
+import StaticLS.IDE.InlayHints.Common (defaultInlayHint)
+import StaticLS.IDE.InlayHints.Types
+import StaticLS.StaticEnv.Options (StaticEnvOptions (..))
+import StaticLS.Monad (StaticLsM)
 
 getInlayHints :: AbsPath -> StaticEnvOptions -> StaticLsM [InlayHint]
 getInlayHints _path _options = do
-  logError "defaultInlayHint " <> defaultInlayHint
-  [defaultInlayHint]
+  case defaultInlayHint.label of
+    Left l -> logError $ "defaultInlayHint " <> (T.pack $ show l)
+    Right (r0 : _rr) -> logError $ "defaultInlayHint " <> r0.value
+  pure [defaultInlayHint]

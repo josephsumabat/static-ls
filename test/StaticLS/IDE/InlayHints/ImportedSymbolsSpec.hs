64cd6cfd92f864f48181f83379fd77bff84cd981
Emma Griffin <emma.audrey.g@gmail.com>
2026-02-09 09:48:13 -0500

    Add tests

---
+++ test/StaticLS/IDE/InlayHints/ImportedSymbolsSpec.hs
@@ -0,0 +1,59 @@
+module StaticLS.IDE.InlayHints.ImportedSymbolsSpec (spec) where
+
+import Data.Map.Strict qualified as Map
+import StaticLS.IDE.InlayHints.ImportedSymbols
+import Test.Hspec
+
+-- $> hspec spec
+spec :: Spec
+spec = do
+  describe "cleanOccName" $ do
+    describe "strips type prefixes" $ do
+      it "strips v: prefix from values" $ do
+        cleanOccName "v:myFunction" `shouldBe` "myFunction"
+
+      it "strips t: prefix from types" $ do
+        cleanOccName "t:MyType" `shouldBe` "MyType"
+
+      it "strips c: prefix from constructors" $ do
+        cleanOccName "c:MyConstructor" `shouldBe` "MyConstructor"
+
+    describe "handles record field selectors" $ do
+      it "converts v: prefixed field selector to Type(..)" $ do
+        cleanOccName "v:fMyType:fieldName" `shouldBe` "MyType(..)"
+
+      it "converts bare field selector to Type(..)" $ do
+        cleanOccName "fSkipActivationItemRequest:skipActivationItemName" `shouldBe` "SkipActivationItemRequest(..)"
+
+      it "does not convert names without colon" $ do
+        cleanOccName "v:fMyFunction" `shouldBe` "fMyFunction"
+
+      it "does not convert names starting with lowercase after f" $ do
+        cleanOccName "v:foo" `shouldBe` "foo"
+
+      it "does not convert bare f followed by lowercase" $ do
+        cleanOccName "foobar" `shouldBe` "foobar"
+
+    describe "preserves regular names" $ do
+      it "preserves names without prefix" $ do
+        cleanOccName "regularName" `shouldBe` "regularName"
+
+  describe "groupByModule" $ do
+    it "groups symbols by module" $ do
+      let symbols =
+            [ ImportedSymbol "v:foo" "Module.A"
+            , ImportedSymbol "v:bar" "Module.B"
+            , ImportedSymbol "t:Baz" "Module.A"
+            ]
+          result = groupByModule symbols
+      Map.lookup "Module.A" result `shouldBe` Just ["foo", "Baz"]
+      Map.lookup "Module.B" result `shouldBe` Just ["bar"]
+
+    it "deduplicates field selectors from same type" $ do
+      let symbols =
+            [ ImportedSymbol "fMyRecord:field1" "Module.A"
+            , ImportedSymbol "fMyRecord:field2" "Module.A"
+            ]
+          result = groupByModule symbols
+      -- Both field selectors should collapse to one MyRecord(..)
+      Map.lookup "Module.A" result `shouldBe` Just ["MyRecord(..)"]
---
+++ test/StaticLS/IDE/InlayHints/ImportsSpec.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module StaticLS.IDE.InlayHints.ImportsSpec (spec) where
+
+import Control.Monad.IO.Class (liftIO)
+import Data.Path qualified as Path
+import Data.Text (Text)
+import Data.Text qualified as T
+import NeatInterpolation
+import StaticLS.IDE.InlayHints.Imports qualified as Imports
+import StaticLS.IDE.InlayHints.Types (InlayHint (..), InlayHintLabelPart (..))
+import StaticLS.StaticEnv.Options qualified as StaticEnv.Options
+import Test.Hspec
+import TestImport.Compilation qualified
+
+spec :: Spec
+spec = do
+  describe "Import inlay hints" $ do
+    it "shows imported symbols for imports without explicit lists" $ do
+      let libPath = "src/Lib.hs"
+          mainPath = "src/Main.hs"
+      TestImport.Compilation.setupCompilation
+        "ImportsSpec-basic"
+        [ ( libPath
+          , [trimming|
+            module Lib where
+
+            myFunction :: Int -> Int
+            myFunction x = x + 1
+
+            anotherFunction :: String -> String
+            anotherFunction s = s ++ "!"
+            |]
+          )
+        , ( mainPath
+          , [trimming|
+            module Main where
+
+            import Lib
+
+            main :: IO ()
+            main = do
+              let x = myFunction 5
+              putStrLn (anotherFunction "hello")
+            |]
+          )
+        ]
+        \dir _sources -> do
+          let mainAbsPath = dir Path.</> mainPath
+          hints <- Imports.getInlayHints mainAbsPath StaticEnv.Options.defaultStaticEnvOptions
+          -- Should have a hint for the Lib import
+          liftIO $ length hints `shouldSatisfy` (>= 1)
+          -- The hint should contain the imported symbols
+          let hintTexts = map getHintText hints
+          liftIO $ any (T.isInfixOf "myFunction") hintTexts `shouldBe` True
+      pure @IO ()
+
+    it "does not show hints for imports with explicit lists" $ do
+      let libPath = "src/Lib.hs"
+          mainPath = "src/Main.hs"
+      TestImport.Compilation.setupCompilation
+        "ImportsSpec-explicit"
+        [ ( libPath
+          , [trimming|
+            module Lib where
+
+            myFunction :: Int -> Int
+            myFunction x = x + 1
+            |]
+          )
+        , ( mainPath
+          , [trimming|
+            module Main where
+
+            import Lib (myFunction)
+
+            main :: IO ()
+            main = print (myFunction 5)
+            |]
+          )
+        ]
+        \dir _sources -> do
+          let mainAbsPath = dir Path.</> mainPath
+          hints <- Imports.getInlayHints mainAbsPath StaticEnv.Options.defaultStaticEnvOptions
+          -- Should not have any hints since import has explicit list
+          liftIO $ length hints `shouldBe` 0
+      pure @IO ()
+
+    it "shows multiple symbols from the same module" $ do
+      let libPath = "src/Lib.hs"
+          mainPath = "src/Main.hs"
+      TestImport.Compilation.setupCompilation
+        "ImportsSpec-multi"
+        [ ( libPath
+          , [trimming|
+            module Lib where
+
+            foo :: Int
+            foo = 1
+
+            bar :: Int
+            bar = 2
+
+            baz :: Int
+            baz = 3
+            |]
+          )
+        , ( mainPath
+          , [trimming|
+            module Main where
+
+            import Lib
+
+            main :: IO ()
+            main = print (foo + bar)
+            |]
+          )
+        ]
+        \dir _sources -> do
+          let mainAbsPath = dir Path.</> mainPath
+          hints <- Imports.getInlayHints mainAbsPath StaticEnv.Options.defaultStaticEnvOptions
+          liftIO $ length hints `shouldSatisfy` (>= 1)
+          let hintTexts = map getHintText hints
+          -- Should contain both foo and bar (but not baz since it's not used)
+          liftIO $ any (T.isInfixOf "foo") hintTexts `shouldBe` True
+          liftIO $ any (T.isInfixOf "bar") hintTexts `shouldBe` True
+      pure @IO ()
+
+getHintText :: InlayHint -> Text
+getHintText hint = case hint.label of
+  Left t -> t
+  Right parts -> T.concat [v | InlayHintLabelPart v _ _ _ <- parts]

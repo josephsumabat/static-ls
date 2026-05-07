ed4260d7c410d1b644660ab0ce32c23c0daa2569
jacobleach-arista <jacob.leach@arista.com>
2026-05-06 20:56:13 -0500

    fix: update nixpkgs pins to support GHC 9.12.2

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -7,6 +7,14 @@ let
     fetchSubmodules = true;
   };
 
+  # GHC 9.12 test fixes merged upstream (well-typed/optics#512) but no
+  # Hackage release yet; tracked in GHC #25631 and GHC #25883
+  optics-repo = {
+    url = "https://github.com/well-typed/optics";
+    sha256 = "sha256-1Eoyaf0KUEtNL48Lb78NTSpx5LJaDDInguea306neXI=";
+    rev = "d3f507217bb828ec97d3ead69b0b4319941c0ec5";
+  };
+
 in
   ghcVersion:
   self: super: {
@@ -15,8 +23,8 @@ in
     all-cabal-hashes =
         # Update revision to match required hackage
         super.fetchurl {
-          url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/59b02844f778d7cc71d2a62ee05c37e17b396051.tar.gz";
-          sha256 = "sha256-NUuQW59vzpXufNpAq4qwx5R0/2TwgDgtapjDSdIybhQ=";
+          url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/e82a78bdc7a453ec96d5d8879bb9862b63a6f1b5.tar.gz";
+          sha256 = "sha256-gV4iJvIfwSSrdYJeHQ6d8qchRP9hRGP2pj8xMkcJoCQ=";
     };
 
     haskellPackages = super.haskell.packages.${self.ghcVersion}.override {
@@ -47,11 +55,17 @@ in
           rev = "ec14d702660c79a907e9c45812958cd0df0f036f";
         }) {};
 
-        hiedb = self.haskell.lib.dontCheck (haskellSuper.callHackage "hiedb" "0.6.0.1" {});
+        hiedb = self.haskell.lib.dontCheck (haskellSuper.callHackage "hiedb" "0.6.0.2" {});
+
         text-rope = haskellSuper.callHackage "text-rope" "0.3" {};
 
-        lsp-types = haskellSuper.callHackage "lsp-types" "2.3.0.0" {};
-        lsp = haskellSuper.callHackage "lsp" "2.7.0.0" {};
+        lsp-types = haskellSuper.callHackage "lsp-types" "2.3.0.1" {};
+
+        lsp = haskellSuper.callHackage "lsp" "2.7.0.1" {};
+
+        optics = self.haskell.lib.dontCheck (
+          haskellSuper.callCabal2nix
+          "optics" "${(super.fetchgit optics-repo)}/optics" {});
       };
     };
   }
